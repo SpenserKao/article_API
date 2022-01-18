86ce45aa6d59de3b232c88fb4bd6e69e3d53322d
Spenser Kao <SpenserKao@Optusnet.com.au>
2022-01-19 02:34:23 +1100

    Delete curlPost.bat
    cleaned up.

--- curlPost.bat
+++
@@ -1,3 +0,0 @@
-#Again, works fine directly under GetBash, but not thru batch file.
-#Solution? Rename the .bat to .sh file, especially when is exuected under gitBash
-curl -i -H "Content-Type: application/json" -X POST -d '{"Id":"13","Title":"Test Article 2 psuedo title","Date":"2022-01-17","Body":"Psuedo body","Tags": ["tag1", "tag2", "tag3"]}' http://localhost:8080/articles
